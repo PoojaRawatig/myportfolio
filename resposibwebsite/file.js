cd53b86023fb0100f558b9749cfabd6627ca21b6
PoojaRawatig123 <poojarawatig1@gmail.com>
2023-08-26 11:19:01 +0530

    first commit

---
+++ resposibwebsite/file.js
@@ -0,0 +1,24 @@
+let header = document.querySelector(header);
+let menu = document.querySelector(#menu-icon);
+let navbar = document.querySelector(.navbar);
+window.addEventListener(scroll, () => {
+  header.classList.toggle(shadow, window.scrollY > 0);
+});
+menu.onclick = () => 
+  navbar.classList.toggle(active);
+;{
+window.onscroll = () =>(
+  navbar.classList.removeactiv);
+};
+// Dark Mode / light mode
+let darkmode = document.querySelector(#darkmode);
+darkmode.onclick = () => {
+  if (darkmode.classList.contains(bx-moon)) {
+    darkmode.classList.replace(bx-moon, bx-sun);
+    document.body.classList.add(active);
+  } else {
+    darkmode.classList.replacebx-sun, bx-moon);
+    document.body.classList.removeactive);
+  }
+};                            
+ 
